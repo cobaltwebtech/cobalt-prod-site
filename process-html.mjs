fbc85a68fa4f62c31a8cddb2b3a5f6007b84faad
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2024-07-19 17:26:28 -0500

    Update process-html.mjs
    Log the build script

--- process-html.mjs
+++ process-html.mjs
@@ -2,6 +2,8 @@ import fs from 'node:fs/promises'
 import { globby } from 'globby'
 import { minify } from 'html-minifier'
 
+console.log('Starting process-html.mjs script...');
+
 // Get all HTML files from the output directory
 const path = './.vercel/output/static'
 const files = await globby(`${path}/**/*.html`)
@@ -20,4 +22,6 @@ await Promise.all(
    	 })
    	 await fs.writeFile(file, html)
     })
-)
+);
+console.log('process-html.mjs script completed.');
+
