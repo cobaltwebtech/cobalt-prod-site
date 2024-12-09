c0daff8844e47571b74bcba4ff75abac9b057436
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2024-12-08 22:18:47 -0600

    Refactored form pages and updates
    Fixed issue with form pages not working with Astro v5. Created new confirmation pages. Updated Astro to v5.0.3 and deps.

--- src/pages/api/sendEmailSupport.ts
+++ src/pages/api/sendEmailSupport.ts
@@ -1,23 +1,23 @@
-export const prerender = false;
-
 import type { APIRoute } from 'astro';
 import { Resend } from 'resend';
 
+export const prerender = false;
+
 export const POST: APIRoute = async ({ request }) => {
-  // Define form inputs here
-  const formData = await request.formData();
-  const supportType = formData.get('support-type');
-  const firstName = formData.get('first-name');
-  const lastName = formData.get('last-name');
-  const email = formData.get('email');
-  const phone = formData.get('phone');
-  const supportMessage = formData.get('support-message');
-
-  // Import Resend API key
-  const resend = new Resend(import.meta.env.RESEND_API_KEY);
-
-  // Email message to Resend
   try {
+    // Define form inputs here
+    const formData = await request.formData();
+    const supportType = formData.get('support-type') as string;
+    const firstName = formData.get('first-name') as string;
+    const lastName = formData.get('last-name') as string;
+    const email = formData.get('email') as string;
+    const phone = formData.get('phone') as string;
+    const supportMessage = formData.get('support-message') as string;
+  
+    // Import Resend API key
+    const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+    // Email message to Resend
     await resend.emails.send({
       from: 'noreply@contact.cobaltweb.tech',
       to: 'info@cobaltweb.tech',
@@ -31,6 +31,9 @@ export const POST: APIRoute = async ({ request }) => {
     }), 
     {
       status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
   } catch (error) {
     console.error('Failed to send email:', error);
@@ -40,6 +43,9 @@ export const POST: APIRoute = async ({ request }) => {
     }), 
     {
       status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
   }
 };
