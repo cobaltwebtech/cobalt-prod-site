f9c8b2312cea2f681510461d3b319dd28681acf9
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2024-12-10 15:08:08 -0600

    Sitemap output directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,13 +10,13 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://www.cobaltweb.tech/",
   prefetch: true,
-  outDir: './dist',
   build: {
     assets: 'assets'
   },
   integrations: [
     tailwind(),
     sitemap({
+      outDir: './dist/client',
       i18n: {
         defaultLocale: "en",
         // All urls that don't contain `fr` after `https://www.cobaltweb.tech/` will be treated as default locale, i.e. `en`
