c48502665aa41b55cfe88b741cab92277fbeb0d2
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2024-12-31 15:45:10 -0600

    Moved plugin to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import { CopyFilesPlugin } from "src/utils/copy-files.ts";
+import { CopyFilesPlugin } from "copy-files";
 
 // https://astro.build/config
 export default defineConfig({
