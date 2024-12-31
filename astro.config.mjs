94cffb8d3e6400727a9b50b3c3cf04025fc4e855
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2024-12-31 15:38:12 -0600

    Set explicit file path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import { CopyFilesPlugin } from "@/copy-files";
+import { CopyFilesPlugin } from "src/utils/copy-files";
 
 // https://astro.build/config
 export default defineConfig({
