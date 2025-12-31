b4f081e73d1313a86e9127b8792da6de45ae2db6
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-12-31 14:01:37 -0600

    Update DSN to Public Value

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/cloudflare";
 export const onRequest = [
   // Make sure Sentry is the first middleware
   Sentry.sentryPagesPlugin((context) => ({
-    dsn: context.env.SENTRY_DSN,
+    dsn: context.env.PUBLIC_SENTRY_DSN,
     // Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
     // Learn more at
     // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,10 +1,16 @@
 import * as Sentry from "@sentry/astro";
 
 // Only initialize in production
-if (import.meta.env.PROD && import.meta.env.SENTRY_DSN) {
+if (import.meta.env.PROD && import.meta.env.PUBLIC_SENTRY_DSN) {
   Sentry.init({
-    dsn: import.meta.env.SENTRY_DSN,
+    dsn: import.meta.env.PUBLIC_SENTRY_DSN,
     sendDefaultPii: true,
     environment: import.meta.env.MODE,
+    integrations: [
+      // send console.log, console.warn, and console.error calls as logs to Sentry
+      Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+    ],
+    // Enable logs to be sent to Sentry
+    enableLogs: true,  
   });
 }
